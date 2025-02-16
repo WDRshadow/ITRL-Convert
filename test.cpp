68c52bde564f4df4dfedfd9d6be1b8dd0411745d
Yunhao Xu <i@wdr.im>
2025-02-16 14:23:57 +0100

    update: add comparing for test

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 
+#include "convert_rgb24_to_yuyv.h"
 #include "convert_rgb24_to_yuyv_cuda.h"
 
 extern "C"
@@ -10,7 +11,7 @@ extern "C"
         unsigned int width = 1920;
         unsigned int height = 1080;
         unsigned char *imageData = new unsigned char[width * height * 3];
-        unsigned char *yuyv422 = nullptr;
+        unsigned char *yuyv422 = new unsigned char[width * height * 2];
         for (int i = 0; i < 100; i++)
         {
             // Randomly set pixels in imageData
@@ -18,22 +19,20 @@ extern "C"
             {
                 imageData[j] = rand() % 256;
             }
-            // CPU time to convert RGB24 to YUYV422
+
+            // with cuda
             auto start1 = std::chrono::high_resolution_clock::now();
-
-            // Allocate memory for YUYV422 data
-            if (yuyv422 == nullptr)
-            {
-                yuyv422 = new unsigned char[width * height * 2];
-            }
-
-            // Convert RGB24 to YUYV422
             convert_rgb24_to_yuyv_cuda(imageData, yuyv422, width, height);
-
-            // CPU time to convert RGB24 to YUYV422 in milliseconds
             auto end1 = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double> elapsed1 = end1 - start1;
-            std::cout << "Conversion time: " << elapsed1.count() * 1000 << " ms" << std::endl;
+            std::cout << "CUDA Conversion time: " << elapsed1.count() * 1000 << " ms" << std::endl;
+
+            // without cuda
+            auto start2 = std::chrono::high_resolution_clock::now();
+            convert_rgb24_to_yuyv(imageData, yuyv422, width, height);
+            auto end2 = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed2 = end2 - start2;
+            std::cout << "Conversion time: " << elapsed2.count() * 1000 << " ms" << std::endl;
         }
     }
 }
