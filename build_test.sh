661938ec0ec88cfc8ba9b077b5ef0a9ca6699687
Yunhao Xu <i@wdr.im>
2025-02-20 11:18:42 +0100

    release: delete test code for main

--- build_test.sh
+++
@@ -1,5 +0,0 @@
-rm test
-rm convert_rgb24_to_yuyv_cuda.o
-
-nvcc -Xcompiler -fPIC -c convert_rgb24_to_yuyv_cuda.cu -o convert_rgb24_to_yuyv_cuda.o
-g++ -I. -lstdc++ -L/usr/local/cuda/lib64 -lcudart -lpthread -o test test.cpp convert_rgb24_to_yuyv_cuda.o -Wl,-rpath,/usr/local/cuda/lib64 -lcudart -lpthread
