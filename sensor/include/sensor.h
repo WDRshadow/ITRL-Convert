068e69a454670bd2b93c7804b5e89dc75c67a898
WDRshadow <i@wdr.im>
2025-03-09 22:04:49 +0100

    update: add sensor api

--- sensor/include/sensor.h
+++ sensor/include/sensor.h
@@ -1,16 +1,21 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+#include <vector>
+
 using namespace std;
 
 class SensorBuffer
 {
+protected:
     vector<float> val;
 
 public:
+    virtual ~SensorBuffer() = default;
+    SensorBuffer() = default;
     explicit SensorBuffer(const string& filename);
     [[nodiscard]] float get_value(int index) const;
-    [[nodiscard]] float get_value() const;
+    [[nodiscard]] virtual float get_value() const;
 };
 
 #endif //SENSOR_H
