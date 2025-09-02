15ced77d49f8443372ce81e4887681549dc46bef
Yunhao Xu <i@wdr.im>
2025-09-02 15:33:05 +0200

    fix: initial buffer bug

--- spinnaker_stream/spinnaker_stream.cpp
+++ spinnaker_stream/spinnaker_stream.cpp
@@ -28,9 +28,6 @@ const int _data_logger_type[] = {_TYPE_INT, _TYPE_FLOAT, _TYPE_FLOAT};
 #define DATA_NUM_2 5
 const int _data_logger_ids_2[] = {PkgNr, RefStrAngle, RefThrottle, RefBrk, Direction};
 const int _data_logger_type_2[] = {_TYPE_INT, _TYPE_FLOAT, _TYPE_FLOAT, _TYPE_FLOAT, _TYPE_INT};
-#define DATA_NUM_3 1
-const int _data_logger_ids_3[] = {Latency};
-const int _data_logger_type_3[] = {_TYPE_FLOAT};
 
 bool is_init = false;
 int video_fd;
@@ -41,22 +38,18 @@ Spinnaker::CameraPtr camera_2 = nullptr;
 Spinnaker::ImagePtr pImage = nullptr;
 SocketBridge *bridge = nullptr;
 SocketBridge *bridge_2 = nullptr;
-SocketBridge *bridge_3 = nullptr;
 char *buffer = nullptr;
 char *buffer_2 = nullptr;
-char *buffer_3 = nullptr;
 bool is_sensor_init = false;
 bool thread_signal = false;
 bool is_thread_running = false;
 bool is_thread_running_2 = false;
-bool is_thread_running_3 = false;
 unsigned char *bayer = nullptr;
 unsigned char *rgb = nullptr;
 unsigned char *new_rgb = nullptr;
 unsigned char *yuyv = nullptr;
 std::thread sensor_thread;
 std::thread sensor_thread_2;
-std::thread sensor_thread_3;
 
 void capture_frames(const char *video_device, const std::string &ip, const int port, bool &signal, const int fps, const int delay_ms, const char *logger, const bool is_hmi, const bool is_p_hmi, const int scale)
 {
@@ -234,7 +227,6 @@ void capture_frames(const char *video_device, const std::string &ip, const int p
     std::unique_ptr<DataLogger> data_logger_2;
     std::shared_mutex bufferMutex;
     std::shared_mutex bufferMutex_2;
-    std::shared_mutex bufferMutex_3;
     std::unique_ptr<RingBuffer> image_buffer = nullptr;
 
     // Initialize Streaming Component
@@ -250,11 +242,6 @@ void capture_frames(const char *video_device, const std::string &ip, const int p
         if (bridge_2)
         {
             is_sensor_connected = is_sensor_connected && bridge_2->isValid();
-        }
-        bridge_3 = new SocketBridge(ip, port + 2);
-        if (bridge_3)
-        {
-            is_sensor_connected = is_sensor_connected && bridge_3->isValid();
         }
     }
     if (is_sensor_connected)
@@ -272,11 +259,6 @@ void capture_frames(const char *video_device, const std::string &ip, const int p
         {
             delete bridge_2;
             bridge_2 = nullptr;
-        }
-        if (bridge_3)
-        {
-            delete bridge_3;
-            bridge_3 = nullptr;
         }
         std::cout << "[spinnaker stream] Sensor data not available." << std::endl;
     }
@@ -399,15 +381,13 @@ void capture_frames(const char *video_device, const std::string &ip, const int p
         {
             if (!is_sensor_init)
             {
-                buffer = new char[BUFFER_SIZE];
-                buffer_2 = new char[BUFFER_SIZE];
-                buffer_3 = new char[BUFFER_SIZE];
+                buffer = new char[BUFFER_SIZE]();
+                buffer_2 = new char[BUFFER_SIZE]();
                 if (is_hmi || is_p_hmi)
                 {
                     vel = std::make_unique<SensorAPI>(Velocity, buffer, BUFFER_SIZE, bufferMutex);
                     ax = std::make_unique<SensorAPI>(AX, buffer, BUFFER_SIZE, bufferMutex);
                     str_whe_phi = std::make_unique<SensorAPI>(RefStrAngle, buffer_2, BUFFER_SIZE, bufferMutex_2);
-                    latency = std::make_unique<SensorAPI>(Latency, buffer_3, BUFFER_SIZE, bufferMutex_3);
                     stream_image = std::make_unique<StreamImage>(orin_width, orin_height);
                     prediction_line = std::make_shared<PredictionLine>("fisheye_calibration.yaml",
                                                                        "homography_calibration.yaml", orin_width, orin_height);
@@ -433,8 +413,6 @@ void capture_frames(const char *video_device, const std::string &ip, const int p
                                             std::ref(thread_signal), std::ref(is_thread_running));
                 sensor_thread_2 = std::thread(receive_data_loop, bridge_2, buffer_2, BUFFER_SIZE, std::ref(bufferMutex_2),
                                               std::ref(thread_signal), std::ref(is_thread_running_2));
-                sensor_thread_3 = std::thread(receive_data_loop, bridge_3, buffer_3, BUFFER_SIZE, std::ref(bufferMutex_3),
-                                              std::ref(thread_signal), std::ref(is_thread_running_3));
                 is_sensor_init = true;
             }
             if (vehicle_direction == FORWARD && (is_hmi || is_p_hmi))
@@ -514,25 +492,23 @@ void capture_frames(const char *video_device, const std::string &ip, const int p
     if (is_sensor_init)
     {
         thread_signal = true;
-        if (sensor_thread.joinable() && sensor_thread_2.joinable() && sensor_thread_3.joinable())
+        if (sensor_thread.joinable() && sensor_thread_2.joinable())
         {
             int count = 0;
-            while ((is_thread_running || is_thread_running_2 || is_thread_running_3) && count++ < 30)
+            while ((is_thread_running || is_thread_running_2) && count++ < 30)
             {
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
-            if (is_thread_running || is_thread_running_2 || is_thread_running_3)
+            if (is_thread_running || is_thread_running_2)
             {
                 std::cerr << "[spinnaker stream] Sensor thread did not exit gracefully" << std::endl;
                 sensor_thread.detach();
                 sensor_thread_2.detach();
-                sensor_thread_3.detach();
             }
             else
             {
                 sensor_thread.join();
                 sensor_thread_2.join();
-                sensor_thread_3.join();
             }
         }
         thread_signal = false;
@@ -540,14 +516,10 @@ void capture_frames(const char *video_device, const std::string &ip, const int p
         buffer = nullptr;
         delete[] buffer_2;
         buffer_2 = nullptr;
-        delete[] buffer_3;
-        buffer_3 = nullptr;
         delete bridge;
         bridge = nullptr;
         delete bridge_2;
         bridge_2 = nullptr;
-        delete bridge_3;
-        bridge_3 = nullptr;
         is_sensor_init = false;
     }
     if (is_init)
